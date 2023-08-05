deabd587b45c87cd4ac411396120a447ebb47725
Ga-Yeong Seo <go26ing@gmail.com>
2023-08-05 22:39:01 +0900

    google search

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230805.222023';
+const cacheName = 'chirpy-20230805.223728';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
